e115f3eee0c2a9e297a95a2f24dcc3efedaa9de9
welpaolo <paolo.sottovia@canonical.com>
2025-06-20 12:29:07 +0200

    [DPE-7437][DO NOT MERGE] Pipeline for Kyuubi tests  (#29)

--- ie-tests/tests.sh
+++ ie-tests/tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+set +x 
 TEST_FOLDER=$1
+TEST_COMMAND=$2
+
+echo ""
 
 echo " "
 echo "Changing folder to ${TEST_FOLDER}"
@@ -14,7 +17,14 @@ mvn clean
 echo " "
 echo " Running tests..."
 # Run maven tests 
-mvn test -fn 
+if [ -z "$TEST_COMMAND" ];
+then 
+    TEST_COMMAND="mvn test -fn"
+fi
+
+echo "Test command: $TEST_COMMAND"
+
+$TEST_COMMAND
 
 echo " "
 echo " Finished."
