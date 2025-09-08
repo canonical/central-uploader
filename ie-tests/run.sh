465d4760938b8cb6e9f9b8544f45cac2b92c86c2
Alex Batisse <alex.batisse@canonical.com>
2025-09-08 10:23:26 +0200

    WIP: Display test logs in GH

--- ie-tests/run.sh
+++ ie-tests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function run_tests(){
+function run_tests() {
     echo "START TEST"
     TEST_COMMAND=$1
     IE_FOLDER=$(pwd)
@@ -19,33 +19,32 @@ function run_tests(){
     STD_ERR="${IE_FOLDER}/logs/${LOG_NAME}.err"
 
     # populate the error file
-    echo "" >> $STD_ERR
+    echo "" >>$STD_ERR
 
-    echo "***************************************" > $STD_OUT
-    echo "     INTEGRATION TESTS" >> $STD_OUT
-    echo "***************************************" >> $STD_OUT
-    echo "" >> $STD_OUT
-    echo "Date: ${DATE}" >> $STD_OUT
-    echo "Branch: ${BRANCH}" >> $STD_OUT
-    echo "Commit: ${COMMIT}" >> $STD_OUT
-    echo "LogName: ${LOG_NAME}" >> $STD_OUT
-    echo "" >> $STD_OUT
-    echo "***************************************" >> $STD_OUT
-    echo "" >> $STD_OUT
+    echo "***************************************" >$STD_OUT
+    echo "     INTEGRATION TESTS" >>$STD_OUT
+    echo "***************************************" >>$STD_OUT
+    echo "" >>$STD_OUT
+    echo "Date: ${DATE}" >>$STD_OUT
+    echo "Branch: ${BRANCH}" >>$STD_OUT
+    echo "Commit: ${COMMIT}" >>$STD_OUT
+    echo "LogName: ${LOG_NAME}" >>$STD_OUT
+    echo "" >>$STD_OUT
+    echo "***************************************" >>$STD_OUT
+    echo "" >>$STD_OUT
 
     FORMAT_TIME="\n***************************************\nTIMINGS:\n%E real\n%U user\n%S sys"
 
     echo "Before running the test!"
-    ./tests.sh "${FOLDER}" "${TEST_COMMAND}" >> $STD_OUT 2> $STD_ERR
+    ./tests.sh "${FOLDER}" "${TEST_COMMAND}"
 
-    echo "***************************************" >> $STD_OUT
+    echo "***************************************" >>$STD_OUT
 }
 
 BRANCH=$1
 TEST_COMMAND=$2
 
-if [ -z "$BRANCH" ];
-then 
+if [ -z "$BRANCH" ]; then
     BRANCH="release-3.3.6-ubuntu2"
 fi
 
