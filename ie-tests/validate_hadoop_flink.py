252c782621ac002bfe395a46037906f5cd0ece66
Alex Batisse <alex.batisse@canonical.com>
2026-06-17 08:48:43 +0200

    [PRA-237] Add flink tests and product release (#42)
    * ci: Add flink tests and product release
    
    * Adapt log parsing
    
    * Split tests in 4 runs, use self-hosted runner
    
    * Fix: Install zip
    
    * Fix: Fix openssl 3 issue with netty tests
    
    * Add connect suite
    
    * Fix log file names
    
    * Shared a single validator between hadoop and flink
    
    * Fix log file name in validation script
    
    * Revert test.sh comment
    
    * ci: Remove pull_request trigger

--- ie-tests/validate_hadoop_flink.py
+++ ie-tests/validate_hadoop_flink.py
@@ -28,7 +28,6 @@ class HadoopLogParser(LogParser):
 
     def parse_log_archive(self, log_archive: str) -> Report:
         """Parse the log archive and extract the test results."""
-        filename = None
         succeeded = 0
         failures = 0
         errors = 0
@@ -45,7 +44,6 @@ class HadoopLogParser(LogParser):
                 for log_file in log_files:
                     if ".out" not in log_file:
                         continue
-                    filename = log_file
                     print("Analyzed log file: " + log_file)
                     file = open(f"{log_directory}/{log_file}", "r")
                     # Read each line in the file
@@ -56,7 +54,7 @@ class HadoopLogParser(LogParser):
                             test_name = clean_line.split("-")[1].strip()
                             executed_modules.append(test_name)
                             if "FAILURE" in clean_line:
-                                failed_tests.append(clean_line.split("-")[1])
+                                failed_tests.append(clean_line.rsplit("-", 1)[1])
                             items = clean_line.split(",")
                             succeeded += int(items[0].split(":")[1])
                             failures += int(items[1].split(":")[1])
@@ -65,7 +63,7 @@ class HadoopLogParser(LogParser):
 
         total = succeeded + failures + errors + skipped
         return Report(
-            log_file=filename,
+            log_file=os.path.basename(log_archive),
             succeeded=succeeded,
             failures=failures,
             errors=errors,
